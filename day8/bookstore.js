e440d8a96a8d76ad4b1d0738ec6e82e326931106
Amr Hassan Hafez <amrhassan98@hotmail.com>
2020-09-01 20:40:41 +0200

    Update bookstore.js

--- day8/bookstore.js
+++ day8/bookstore.js
@@ -131,18 +131,35 @@ console.log(author.name);
 //   }
 // });
 //
-Author.
-find().
-populate('author').
-exec(function (err, stories) {
-if (err){
-   return handleError(err);
- }else{
-   for(i=0;i<stories.length;i++){
-       console.log('The author is %s', stories[i].name);
-       // prints "The author is Ian Fleming"
-   }
- }
+// Author.
+// find({name: 'Ronaldo'}).
+// populate('author').
+// exec(function (err, stories) {
+// if (err){
+//    return handleError(err);
+//  }else{
+//    for(i=0;i<stories.length;i++){
+//        console.log('The author is %s', stories[i].name);
+//        // prints "The author is Ian Fleming"
+//    }
+//  }
+// });
+
+// Story.
+//   findOne({ title: 'Casino Royale' }).
+//   populate('author').
+//   exec(function (err, story) {
+//     if (err) return handleError(err);
+//     console.log('The author is ( Populate test )%s', story.author.name);
+//     // prints "The author is Ian Fleming"
+//   });
+
+Story.findOne({ title: 'Casino Royale' }, function(error, story) {
+if (error) {
+  return handleError(error);
+}
+story.author = author;
+console.log(story.author.name); // prints "Ian Fleming"
 });
 
 // Author.deleteOne({name:'test123'}, function(err){
@@ -152,6 +169,48 @@ if (err){
 //     console.log("Done");
 //   }
 // });
+
+// ------------------------
+
+// const author_new = new Author({
+//    _id: new mongoose.Types.ObjectId(),
+//    name: 'Spartaks',
+//    age: 50,
+//    stories: []
+//
+//  });
+//  console.log(author_new.name);
+//
+//  author_new.save(function (err) {
+//    if (err){
+//      console.log(error);
+//    }else{
+//     console.log("Added new author")
+// }
+// });
+//
+//    const story_new = new Story({
+//      title: 'Casino Royale',
+//      author: author_new._id    // assign the _id from the person
+//    });
+//
+//    story_new.save(function (err) {
+//       if (err){
+//        console.log(error);
+//      }else{
+//       console.log("Added new story")
+//   }
+// });
+//      console.log(story1.title+" author id: "+story1.author);
+//
+//
+//  Author.create([
+//    {name:"fan1", age:20, _id:1},
+//    {name:"fan2", age:20, _id:2},
+//    {name:"fan3", age:20, _id:3},
+//    {name:"fan4", age:20, _id:4},
+//  ]);
+
 
 
 
